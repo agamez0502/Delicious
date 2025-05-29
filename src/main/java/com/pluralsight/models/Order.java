81e89ab5b7ee2d8ce06b6bfe8004d385c95b2b31
Alondra Gamez <agamez@appdev.yearup.org>
2025-05-29 13:58:13 -0500

    Completed Order class

--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -4,27 +4,101 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    // create List for order items (sandwiches, drinks, and chips)
-    // store total price
 
-    // constructor:
-    //   - initialize the class propeties
+    //properties=================================================================================
+    //create List for order items (sandwiches, drinks, and chips)
+    private List<Torta> tortas  = new ArrayList<>();
+    private List<Drink> drinks = new ArrayList<>();
+    private List<Chips> chips = new ArrayList<>();
 
-    // addSandwich method:
-    //   - add sandwich to list
+    //store total price
 
-    // addDrink method:
-    //   - add drink to list
+    //constructor================================================================================
+    //initialize the class properties
+    public Order(List<Torta> tortas, List<Drink> drinks, List<Chips> chips) {
+        this.tortas = tortas;
+        this.drinks = drinks;
+        this.chips = chips;
+    }
 
-    // addChips method:
-    //   - add chips to list
+    //methods====================================================================================
+    //addSandwich method that adds sandwich to list
+    public void addTorta(Torta torta){
+        tortas.add(torta);
+    }
 
-    // getTotal method:
-    //   - return total price of order
+    //addDrink method that adds drink to list
+    public void addDrink(Drink drink){
+        drinks.add(drink);
+    }
 
-    // getSandwiches, getDrinks, getChips:
-    //   - return the orders items
+    // addChips method that adds chips to list
+    public void addChips(Chips chip){
+        chips.add(chip);
+    }
 
-    // getOrderSummary method:
-    //   - return formatted string of all items and total
+    // getTotal method that returns total price of order
+    public double getTotal(){
+        double total = 0;
+
+        for(Torta torta : tortas){
+            total = total + torta.getPrice();
+        }
+        for(Drink drink : drinks){
+            total = total + drink.getPrice();
+        }
+        for(Chips chip : chips){
+            total = total + chip.getPrice();
+        }
+        return total;
+    }
+
+    // getOrderSummary method that returns formatted string of all items and total
+    public getOrderSummary(){
+        String summary = "";
+
+        summary = summary + "Tortas:\n";
+        for(Torta torta : tortas){
+            summary = summary + torta.getSummary() + "\n";
+        }
+
+        summary = summary + "Drinks:\n";
+        for(Drink drink : drinks){
+            summary = summary + drink.toString() + "\n";
+        }
+
+        summary = summary + "Chips:\n";
+        for(Chips chip : chips){
+            summary = summary + chip.toString() + "\n";
+
+        }
+        summary = summary + String.format("Total: $%.2f\n", getTotal());
+        return summary;
+    }
+
+    //getters and setters========================================================================
+    // getSandwiches, getDrinks, getChips- returns the orders items
+    public List<Torta> getTortas() {
+        return tortas;
+    }
+
+    public void setTortas(List<Torta> tortas) {
+        this.tortas = tortas;
+    }
+
+    public List<Drink> getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(List<Drink> drinks) {
+        this.drinks = drinks;
+    }
+
+    public List<Chips> getChips() {
+        return chips;
+    }
+
+    public void setChips(List<Chips> chips) {
+        this.chips = chips;
+    }
 }
