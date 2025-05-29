3ba9198f74254a3472aff636eaca2556d24b360c
Alondra Gamez <agamez@appdev.yearup.org>
2025-05-29 16:31:37 -0500

    Add: Completed Chips class

--- src/main/java/com/pluralsight/models/Chips.java
+++ src/main/java/com/pluralsight/models/Chips.java
@@ -1,4 +1,36 @@
 package com.pluralsight.models;
 
 public class Chips {
+    //properties=================================================================================
+    private String name;
+    private double price;
+
+    //constructor================================================================================
+    public Chips(String name) {
+        this.name = name;
+        this.price = 1.50;
+    }
+
+    //methods====================================================================================
+    @Override
+    public String toString() {
+        return name + " - $" + String.format("%.2f", price);
+    }
+
+    // getters and setters========================================================================
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
