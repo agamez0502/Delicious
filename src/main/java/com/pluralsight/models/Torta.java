4c293b6fa779427a21ca34339c5075af52bbd1ba
Alondra Gamez <agamez@appdev.yearup.org>
2025-06-01 17:15:45 -0500

    lost code but found it in GitHub and added it to Torta class with updated code - string builder and implementation

--- src/main/java/com/pluralsight/models/Torta.java
+++ src/main/java/com/pluralsight/models/Torta.java
@@ -1,16 +1,13 @@
 package com.pluralsight.models;
 
+import com.pluralsight.ui.OrderItem;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Torta {
+public class Torta implements OrderItem {
 
     //properties=================================================================================
-    //store bread type (white, wheat, rye, wrap)
-    //store sandwich size (4, 8, or 12 inches)
-    //store if sandwich is toasted
-    //store meats, cheeses, toppings, sauces
-    //keep track of total price
     private String size;
     private String breadType;
     private boolean isToasted;
@@ -22,19 +19,17 @@ public class Torta {
     private double totalPrice;
 
     //constructor================================================================================
-    //takes bread type, size, toasted
-    //initialize properties
     public Torta(String size, String breadType, boolean isToasted) {
         this.size = size;
         this.breadType = breadType;
         this.isToasted = isToasted;
 
         //set base price based on size
-        if(size.equalsIgnoreCase("chico")){
+        if (size.equalsIgnoreCase("Chico")) {
             basePrice = 5.50;
-        } else if (size.equalsIgnoreCase("mediano")) {
+        } else if (size.equalsIgnoreCase("Mediano")) {
             basePrice = 7.00;
-        } else if (size.equalsIgnoreCase("grande")) {
+        } else if (size.equalsIgnoreCase("Grande")) {
             basePrice = 8.50;
         }
         totalPrice = basePrice;
@@ -43,121 +38,118 @@ public class Torta {
     //methods====================================================================================
     //addProtein method that adds meat(s), what is the price based on size and whether it's extra?
     public void addProtein(String name, boolean isExtra) {
-        Toppings topping = new Toppings(name, "protein", isExtra);
+        Toppings topping = new Toppings(name, "Protein", isExtra);
         proteins.add(topping);
 
-        if (size.equalsIgnoreCase("chico")) {
-            if(isExtra){
-                totalPrice = totalPrice + 0.50;
+        if (size.equalsIgnoreCase("Chico")) {
+            if (isExtra) {
+                totalPrice += 0.50;
             } else {
-                totalPrice = totalPrice + 1.00;
+                totalPrice += 1.00;
             }
-        } else if (size.equalsIgnoreCase("mediano")) {
-            if(isExtra){
-                totalPrice = totalPrice + 1.00;
+        } else if (size.equalsIgnoreCase("Mediano")) {
+            if (isExtra) {
+                totalPrice += 1.00;
             } else {
-                totalPrice = totalPrice + 2.00;
+                totalPrice += 2.00;
             }
-        } else if (size.equalsIgnoreCase("grande")) {
-            if (isExtra){
-                totalPrice = totalPrice + 1.50;
+        } else if (size.equalsIgnoreCase("Grande")) {
+            if (isExtra) {
+                totalPrice += 1.50;
             } else {
-                totalPrice = totalPrice + 3.00;
+                totalPrice += 3.00;
             }
         }
     }
 
     //addCheese method that adds cheese(s) and price depending on size and whether it's extra
     public void addCheese(String name, boolean isExtra) {
-        Toppings topping = new Toppings(name, "cheese", isExtra);
+        Toppings topping = new Toppings(name, "Cheese", isExtra);
         cheeses.add(topping);
 
-        if (size.equalsIgnoreCase("chico")) {
-            if(isExtra){
-                totalPrice = totalPrice + 0.30;
+        if (size.equalsIgnoreCase("Chico")) {
+            if (isExtra) {
+                totalPrice += 0.30;
             } else {
-                totalPrice = totalPrice + 0.75;
+                totalPrice += 0.75;
             }
-        } else if (size.equalsIgnoreCase("mediano")) {
-            if(isExtra){
-                totalPrice = totalPrice + 0.60;
+        } else if (size.equalsIgnoreCase("Mediano")) {
+            if (isExtra) {
+                totalPrice += 0.60;
             } else {
-                totalPrice = totalPrice + 1.50;
+                totalPrice += 1.50;
             }
-        } else if (size.equalsIgnoreCase("grande")) {
-            if (isExtra){
-                totalPrice = totalPrice + 0.90;
+        } else if (size.equalsIgnoreCase("Grande")) {
+            if (isExtra) {
+                totalPrice += 0.90;
             } else {
-                totalPrice = totalPrice + 2.25;
+                totalPrice += 2.25;
             }
         }
     }
 
     //addVeggies method that adds veggie(s) at no charge
     public void addVeggie(String name) {
-        Toppings topping = new Toppings(name, "veggie", false);
+        Toppings topping = new Toppings(name, "Veggie", false);
         veggies.add(topping);
     }
 
     //addSauce method that adds sauce(s) at no charge
     public void addSauce(String name) {
-        Toppings topping = new Toppings(name, "sauce", false);
+        Toppings topping = new Toppings(name, "Sauce", false);
         sauces.add(topping);
     }
 
-    // getPrice method that returns current total price
+    //implement from OrderItem - getPrice method that returns current total price
+    @Override
     public double getPrice() {
         return totalPrice;
     }
 
-    // getSummary method that returns a string with all sandwich details and price
+    //implement from OrderItem - getSummary method that returns a string with all sandwich details and price
+    @Override
     public String getSummary() {
-        String summary = "";
+        StringBuilder summary = new StringBuilder();
 
-        summary = summary + "Size: " + size + "\n";
-        summary = summary + "Bread: " + breadType + "\n";
+        summary.append("\uD83E\uDD6A Size: ").append(size).append("\n");
+        summary.append("\uD83C\uDF5E Bread: ").append(breadType).append("\n");
+        summary.append("\uD83D\uDD25 Toasted: ").append(isToasted ? "Yes ✅" : "No ❌").append("\n");
 
-        if(isToasted){
-            summary = summary + "Toasted: Yes\n";
-        } else{
-            summary = summary + "Toasted: No\n";
-        }
-
-        summary = summary + "Proteins: ";
-        for(Toppings t : proteins) {
-            summary = summary + t.getName();
-            if(t.isExtra()) {
-                summary = summary + " (extra)";
+        summary.append("🍖 Proteins: ");
+        for (Toppings t : proteins) {
+            summary.append(t.getName());
+            if (t.isExtra()) {
+                summary.append(" (extra)");
             }
-            summary = summary + ", ";
+            summary.append(", ");
         }
-        summary = summary + "\n";
+        summary.append("\n");
 
-        summary = summary + "Cheeses: ";
-        for(Toppings t : cheeses) {
-            summary = summary + t.getName();
-            if(t.isExtra()) {
-                summary = summary + " (extra)";
+        summary.append("🧀 Cheeses: ");
+        for (Toppings t : cheeses) {
+            summary.append(t.getName());
+            if (t.isExtra()) {
+                summary.append(" (extra)");
             }
-            summary = summary + ", ";
+            summary.append(", ");
         }
-        summary = summary + "\n";
+        summary.append("\n");
 
-        summary += "Veggies: ";
+        summary.append("🥬 Veggies: ");
         for (Toppings t : veggies) {
-            summary = summary + t.getName() + ", ";
+            summary.append(t.getName()).append(", ");
         }
-        summary = summary + "\n";
+        summary.append("\n");
 
-        summary += "Sauces: ";
+        summary.append("🌶️ Sauces: ");
         for (Toppings t : sauces) {
-            summary = summary + t.getName() + ", ";
+            summary.append(t.getName()).append(", ");
         }
-        summary = summary + "\n";
+        summary.append("\n");
 
-        summary =  summary + "Torta Price: $" + String.format("%.2f", totalPrice);
+        summary.append("💲 Torta Price: $").append(String.format("%.2f", totalPrice));
 
-        return summary;
+        return summary.toString();
     }
 
     //getters and setters========================================================================
