e610e64171029bf7b5594257c0b72a09b4767c7c
Alondra Gamez <agamez@appdev.yearup.org>
2025-06-01 22:23:49 -0500

    Fixed Chip class - implements OrderItem and fixed methods and pricing since the chip price is 1.50 for any chip

--- src/main/java/com/pluralsight/models/Chips.java
+++ src/main/java/com/pluralsight/models/Chips.java
@@ -1,33 +1,38 @@
 package com.pluralsight.models;
 
-public class Chips {
+import com.pluralsight.ui.OrderItem;
+
+public class Chips implements OrderItem {
+
     //properties=================================================================================
-    private String name;
-    private double price;
+    private String flavor;
+    private double price = 1.50; //fixed price
 
     //constructor================================================================================
-    public Chips(String name) {
-        this.name = name;
-        this.price = 1.50;
+    public Chips(String flavor) {
+        this.flavor = flavor;
     }
 
     //methods====================================================================================
+    //implement from OrderItem - getPrice method that returns current total price
     @Override
-    public String toString() {
-        return name + " - $" + String.format("%.2f", price);
+    public double getPrice() {
+        return price;
+    }
+
+    //implement from OrderItem - getSummary method that returns a string with all chip details and price
+    @Override
+    public String getSummary() {
+        return "\uD83C\uDF5F Chips: " + flavor + " - $" + String.format("%.2f", price);
     }
 
     // getters and setters========================================================================
-    public String getName() {
-        return name;
+    public String getFlavor() {
+        return flavor;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
     }
 
     public void setPrice(double price) {
--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -14,6 +14,7 @@ public class Drink implements OrderItem {
         this.size = size;
         this.flavor = flavor;
 
+        //set base price based on size
         System.out.println("1) Chico");
         System.out.println("2) Mediano");
         System.out.println("3) Grande");
