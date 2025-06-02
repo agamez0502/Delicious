f1c2b116b960406e7bac5f54ccf62d3d4e8fb767
Alondra Gamez <agamez@appdev.yearup.org>
2025-06-02 05:15:55 -0500

    Created ToppingSelection class to help me add topping in my userinterface class specifically for the proteins and cheeses!

---
+++ src/main/java/com/pluralsight/models/ToppingSelection.java
@@ -0,0 +1,37 @@
+package com.pluralsight.models;
+
+public class ToppingSelection {
+
+    //properties=================================================================================
+    private String name;
+    private boolean isExtra;
+
+    //constructor================================================================================
+    public ToppingSelection(String name, boolean isExtra) {
+        this.name = name;
+        this.isExtra = isExtra;
+    }
+
+    //methods====================================================================================
+    @Override
+    public String toString(){
+        return name + (isExtra ? " (extra)" : "");
+    }
+
+    //getters and setters========================================================================
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isExtra() {
+        return isExtra;
+    }
+
+    public void setExtra(boolean extra) {
+        isExtra = extra;
+    }
+}
